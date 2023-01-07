a844b236d9571b784941687d4db4555ec61169ff
jebastine1997 <jebastine.anthony@gmail.com>
2023-01-07 19:30:28 +0530

    3rd comit

--- did_django_nasa_api/settings.py
+++ did_django_nasa_api/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = 'django-insecure-$#=yel7mr9+bo!-vh^nnzwpt50ktx*s23x12m!_p6w(=tir=@6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -131,11 +131,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+
 
 API_KEY = 'ongVqC5PHohwGO6m6uwdoF2dl2j4McJLn7VRtW4E'
 
